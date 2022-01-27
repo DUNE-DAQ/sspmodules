59a516f29274b42e46f2cbcd48c26353c5263e0e
Michael Hudson Kirby <kirby@np04-srv-012.cern.ch>
2022-01-27 17:23:39 +0100

    Second commit for the SSPLEDCalibModule plugin and SSPLEDCalibWrapper module

---
+++ plugins/SSPLEDCalibModule.cpp
@@ -0,0 +1,145 @@
+/**
+ * @file SSPLEDCalibModule.cpp SSPLEDCalibModule DAQModule implementation
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+#ifndef SSPMODULES_PLUGINS_SSPLEDCALIBMODULE_CPP_
+#define SSPMODULES_PLUGINS_SSPLEDCALIBMODULE_CPP_
+
+#include "logging/Logging.hpp"
+
+#include "sspmodules/sspledcalibmodule/Nljs.hpp"
+
+#include "SSPLEDCalibModule.hpp"
+//#include "SspIssues.hpp"
+
+#include <chrono>
+#include <memory>
+#include <string>
+#include <thread>
+#include <utility>
+#include <vector>
+
+/**
+ * @brief Name used by TRACE TLOG calls from this source file
+ */
+#define TRACE_NAME "SSPLEDCalibModule" // NOLINT
+
+/**
+ * @brief TRACE debug levels used in this source file
+ */
+
+enum
+{
+  TLVL_ENTER_EXIT_METHODS = 5,
+  TLVL_WORK_STEPS = 10,
+  TLVL_BOOKKEEPING = 15,
+  TLVL_FULL_DEBUG = 63
+};
+
+namespace dunedaq {
+namespace sspmodules {
+
+SSPLEDCalibModule::SSPLEDCalibModule(const std::string& name)
+  : DAQModule(name)
+  , m_configured(false)
+  , m_card_id(0)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule constructor called.";
+  m_card_wrapper = std::make_unique<SSPLEDCalibWrapper>();
+
+  register_command("conf", &SSPLEDCalibModule::do_configure);
+  register_command("start", &SSPLEDCalibModule::do_start);
+  register_command("stop", &SSPLEDCalibModule::do_stop);
+  register_command("start_pulses", &SSPLEDCalibModule::do_start_pulses);
+  register_command("stop_pulses", &SSPLEDCalibModule::do_stop_pulses);
+
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule constructor complete.";
+}
+
+inline void
+tokenize(std::string const& str, const char delim, std::vector<std::string>& out)
+{
+  std::size_t start;
+  std::size_t end = 0;
+  while ((start = str.find_first_not_of(delim, end)) != std::string::npos) {
+    end = str.find(delim, start);
+    out.push_back(str.substr(start, end - start));
+  }
+}
+
+void
+SSPLEDCalibModule::init(const data_t& args)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule init called.";
+  auto ini = args.get<appfwk::app::ModInit>();
+  m_card_wrapper->init(args);
+  for (const auto& qi : ini.qinfos) {
+    if (qi.dir != "output") {
+      // ers::error(InitializationError(ERS_HERE, "Only output queues are supported in this module!"));
+      continue;
+    } else {
+      TLOG_DEBUG(TLVL_WORK_STEPS) << ": SSPLEDCalibModule output queue is " << qi.inst;
+    }
+  }
+
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule init complete.";
+  // Set function for the SSPLEDCalibWrapper's block processor.
+  // m_card_wrapper->set_block_addr_handler(m_block_router);
+}
+
+void
+SSPLEDCalibModule::do_configure(const data_t& args)
+{
+
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule do_configure called.";
+  if (!m_configured) {
+    TLOG_DEBUG(TLVL_WORK_STEPS) << ": SSPLEDCalibModule not yet configured so digging in.";
+    m_cfg = args.get<dunedaq::sspmodules::sspledcalibmodule::Conf>();
+    m_card_id = m_cfg.card_id;
+    m_card_wrapper->configure(args);
+    TLOG_DEBUG(TLVL_WORK_STEPS) << ": SSPLEDCalibModule do_configure coming back from SSPLEDCalibWrapper configure.";
+    m_configured = true;
+  } else {
+    TLOG_DEBUG(TLVL_WORK_STEPS) << ": SSPLEDCalibModule is already configured so not going to configure again.";
+  }
+
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule do_configure complete.";
+}
+
+void
+SSPLEDCalibModule::do_start_pulses(const data_t& args, int pulse_count)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule do_start called.\n NOTE THAT THIS DOES NOT DO ANYTHING!!!";
+  m_card_wrapper->start(args);
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule do_start complete.";
+}
+
+void
+SSPLEDCalibModule::do_start(const data_t& args)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule do_start called.\n NOTE THAT THIS DOES NOT DO ANYTHING!!!";
+  m_card_wrapper->start(args);
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule do_start complete.";
+}
+
+void
+SSPLEDCalibModule::do_stop(const data_t& args)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule do_stop called.\n NOTE THAT THIS DOES NOT DO ANYTHING!!!";
+  m_card_wrapper->stop(args);
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << "SSPLEDCalibModule do_stop complete.";
+}
+
+void
+SSPLEDCalibModule::get_info(opmonlib::InfoCollector& /*ci*/, int /*level*/)
+{}
+
+} // namespace sspmodules
+} // namespace dunedaq
+
+DEFINE_DUNE_DAQ_MODULE(dunedaq::sspmodules::SSPLEDCalibModule)
+
+#endif // SSPMODULES_PLUGINS_SSPLEDCALIBMODULE_CPP_
---
+++ src/SSPLEDCalibWrapper.hpp
@@ -0,0 +1,112 @@
+/**
+ * @file SSPLEDCalibWrapper.hpp SSP library wrapper
+ *
+ * This is part of the DUNE DAQ , copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+#ifndef SSPMODULES_SRC_SSPLEDCALIBWRAPPER_HPP_
+#define SSPMODULES_SRC_SSPLEDCALIBWRAPPER_HPP_
+
+#include "sspmodules/sspledcalibmodule/Nljs.hpp"
+
+#include "detdataformats/ssp/SSPTypes.hpp"
+
+#include "SSPIssues.hpp"
+#include "anlBoard/DeviceInterface.hpp"
+#include "logging/Logging.hpp"
+#include "readout/utils/ReusableThread.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <atomic>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace dunedaq {
+namespace sspmodules {
+
+class SSPLEDCalibWrapper
+{
+public:
+  /**
+   * @brief SSPLEDCalibWrapper Constructor
+   * @param name Instance name for this CardWrapper instance
+   */
+  SSPLEDCalibWrapper();
+  ~SSPLEDCalibWrapper();
+  SSPLEDCalibWrapper(const SSPLEDCalibWrapper&) = delete;            ///< SSPLEDCalibWrapper is not copy-constructible
+  SSPLEDCalibWrapper& operator=(const SSPLEDCalibWrapper&) = delete; ///< SSPLEDCalibWrapper is not copy-assignable
+  SSPLEDCalibWrapper(SSPLEDCalibWrapper&&) = delete;                 ///< SSPLEDCalibWrapper is not move-constructible
+  SSPLEDCalibWrapper& operator=(SSPLEDCalibWrapper&&) = delete;      ///< SSPLEDCalibWrapper is not move-assignable
+
+  using data_t = nlohmann::json;
+  void init(const data_t& args);
+  void configure(const data_t& args);
+  void start(const data_t& args);
+  void stop(const data_t& args);
+  //void set_running(bool should_run);
+  void start_pulses(const data_t& args);
+  void stop_pulses(const data_t& args);
+
+private:
+  // Types
+  using module_conf_t = dunedaq::sspmodules::sspledcalibmodule::Conf;
+
+  // these are SSP configurations for this instance of the SSP LED Calib Wrapper
+  // dune::detail::FragmentType const fragment_type_; // Type of fragment (see FragmentType.hh), //KIRBY this is
+  // something I don't know how to transition to the new framework
+  dunedaq::sspmodules::DeviceInterface* m_device_interface; // instance of the SSP device interface class
+  readout::ReusableThread m_ssp_processor; // reusable thread used to start the work done by the device interface
+
+  // module status booleans for transition like init, conf, start, etc
+  std::atomic<bool> m_run_marker;
+  std::atomic<bool> m_configure;
+
+  // all of the configure variables for the SSP
+  module_conf_t m_cfg;
+
+  // Initialization configuration variables
+  unsigned int m_board_id {0};  // this is the ID of the SSP board
+  unsigned long m_board_ip; // this is the ip address of the SSP board // NOLINT
+  unsigned int m_partition_number {0};
+  unsigned int m_timing_address {0};
+  unsigned long m_module_id; // NOLINT
+
+  // DAQ configuration variables first
+  unsigned int m_pre_trig_length {0}; // Window length in ticks for packets to be included in a fragment. This is the length
+                                  // of the window before the trigger timestamp.
+  unsigned int m_post_trig_length {0};       // Length of the window after the trigger timestamp.
+  unsigned int m_use_external_timestamp {2}; // Whether to use the external timestamp to determine whether to include
+                                         // packets in fragment. Both timestamps are stored in the packets anyway. 0 is
+                                         // front panel, 1 is NOvA style
+  unsigned int m_trigger_write_delay {0};
+  unsigned int m_trigger_latency {0};
+  int m_dummy_period {-1};
+  unsigned int m_hardware_clock_rate_in_MHz {0}; // clock rate on the hardware in MHz
+  unsigned int m_trigger_mask {0};     // this is normally given as a hex number for what triggers to mask on or off
+  int m_fragment_timestamp_offset {0}; // offset for the timestamp put into the data stream of this SSP
+
+  // Tracking metrics for debugging and checking consistency
+  unsigned long m_num_zero_fragments;   // NOLINT
+  unsigned long m_num_fragments_sent;   // NOLINT
+  unsigned long m_num_read_event_calls; // NOLINT
+  std::string m_instance_name_for_metrics;
+
+  // Card
+  void open_card();
+  void close_card();
+  void configure_daq(const data_t& args);
+  void configure_device(const data_t& args);
+  void build_channel_control_registers(const std::vector<std::pair<std::string, unsigned int>> entries,
+                                       std::vector<unsigned int>& reg);
+  void process_ssp();
+};
+
+} // namespace sspmodules
+} // namespace dunedaq
+
+#endif // SSPMODULES_SRC_SSPLEDCALIBWRAPPER_HPP_
